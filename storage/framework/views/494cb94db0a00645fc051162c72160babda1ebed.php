6507192b60f5014fa9234ad7442b6ecb6ce37957
jp-avr <joaopedroazevedoreisveras@gmail.com>
2023-10-11 10:59:09 -0300

    subindo

---
+++ storage/framework/views/494cb94db0a00645fc051162c72160babda1ebed.php
@@ -0,0 +1,3 @@
+
+<h1>Sobre nós</h1>
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\wamp64\www\app_super_gestao\resources\views/sobrenos.blade.php ENDPATH**/ ?>
