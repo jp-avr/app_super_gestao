91b1e03b2ab8b14f1998e9ec4fcdd25ba960829b
jp-avr <joaopedroazevedoreisveras@gmail.com>
2023-10-11 16:11:46 -0300

    adicionando imagens e css

--- resources/views/sobre/index.blade.php
+++ resources/views/sobre/index.blade.php
@@ -1,2 +1,47 @@
-@extends ('layouts.app')
-<h1>Sobre nós</h1>
+@extends('layouts.app')
+
+
+<div class="topo">
+
+    <div class="logo">
+        <img src="img/logo.png">
+    </div>
+
+    <div class="menu">
+        <ul>
+            <li><a href="{{ route('home') }}">Principal</a></li>
+            <li><a href="{{ route('sobrenos') }}">Sobre Nós</a></li>
+            <li><a href="{{ route('contatos') }}">Contato</a></li>
+        </ul>
+    </div>
+</div>
+
+<div class="conteudo-pagina">
+    <div class="titulo-pagina">
+        <h1>Olá, eu sou o Super Gestão</h1>
+    </div>
+
+    <div class="informacao-pagina">
+        <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+        <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+    </div>  
+</div>
+
+<div class="rodape">
+    <div class="redes-sociais">
+        <h2>Redes sociais</h2>
+        <img src="img/facebook.png">
+        <img src="img/linkedin.png">
+        <img src="img/youtube.png">
+    </div>
+    <div class="area-contato">
+        <h2>Contato</h2>
+        <span>(98) 99111-9229</span>
+        <br>
+        <span>joaopedroazevedoreisveras@gmail.com</span>
+    </div>
+    <div class="localizacao">
+        <h2>Localização</h2>
+        <img src="img/mapa.png">
+    </div>
+</div>
